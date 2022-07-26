d7835ac326a0c44c474c50d68ae075ca788d6ca3
Martín (Netux) Rodríguez <send.email@netux.site>
2022-07-26 17:38:34 -0300

    WEBAPP: Ensure app loop is set as current event loop

--- WEBAPP.py
+++ WEBAPP.py
@@ -212,6 +212,7 @@ async def remove_scheduled_loop():
 
 if __name__ == "__main__":
 	loop = asyncio.new_event_loop()
+	asyncio.set_event_loop(loop)
 
 	loop.create_task(do_load())
 	loop.create_task(remove_scheduled_loop())
